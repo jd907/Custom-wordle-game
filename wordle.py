daf5b9e2025fb93d730cc3c93f08a0601bccfc89
jd907 <75789003+jd907@users.noreply.github.com>
2023-08-20 09:50:30 -0800

    Refactor game logic and add class `LettersResult`
    - Refactored the Wordle class to implement the game logic.
    - Added a new class `LettersResult` to represent the state of each letter in the game.
    - Updated the `attempt` method to validate the length of the word.
    - Updated the `check_letters` method to use a set for faster lookup of in_word letters.
    - Added properties `is_solved`, `remaining_attempts`, and `can_attempt` to provide game state information.
    
    Refactors game logic and introduces `LettersResult`
    
    The game logic has been refactored improving performance and maintainability. A `LettersResult` class was added to better manage letter state during gameplay. The `attempt` method now validates word length enhancing error handling. The `check_letters` method has been optimized using sets for improved lookup speed. New properties have been introduced to enable better tracking of the game state including `is_solved`, `remaining_attempts`, and `can_attempt`.

--- wordle.py
+++ wordle.py
@@ -1,5 +1,14 @@
-class LettersResult:
+from typing import List
 
+
+class LettersResult:
+    """Represents a letter's state in the game of Wordle.
+
+    Attributes:
+        character (str): The letter character.
+        in_word (bool): Indicates if the letter is in the secret word but not in the correct position.
+        in_position (bool): Indicates if the letter is in the correct position in the secret word.
+    """
     def __init__(self, character: str):
         self.character: str = character
         self.in_word: bool = False
@@ -10,25 +19,48 @@ class LettersResult:
 
 
 class Wordle:
+    """Implements the Wordle game logic.
 
+    Attributes:
+        secret_word (str): The secret word to be guessed.
+        word_length (int): The length of the secret word.
+        max_attempts (int): The maximum number of attempts allowed.
+        attempts (List[str]): The list of attempted words.
+
+    Methods:
+        attempt: Record an attempt with the given word.
+        check_letters: Check the letters in the word against the secret word.
+        is_solved: Check if the word has been solved.
+        remaining_attempts: Calculate the remaining attempts.
+        can_attempt: Check if another attempt can be made.
+    """
     def __init__(self, secret_word: str, word_length: int, max_attempts: int):
         self.secret_word: str = secret_word.upper()
         self.word_length = word_length
         self.max_attempts = max_attempts
-        self.attempts = []
+        self.attempts: List[str] = []
 
-    # attempt
-    def attempt(self, word: str):
+    def attempt(self, word: str) -> None:
+        """Record an attempt with the given word."""
+        if len(word) != self.word_length:
+            raise ValueError("Invalid word length")
         word = word.upper()
         self.attempts.append(word)
 
-    # guess
-    def check_letters(self, word: str) -> list:
+    def check_letters(self, word: str) -> List[LettersResult]:
+        """Check the letters in the word against the secret word.
+
+        :param word: The word to check.
+        :return: A list of LetterResults.
+        """
+        if len(word) != self.word_length:
+            raise ValueError("Invalid word length")
+
         word = word.upper()
         result = [LettersResult(character) for character in word]
         secret = list(self.secret_word)
 
-        # check for green letters
+        # check for in_position (green) letters
         for i in range(self.word_length):
             letter = result[i]
             if letter.character == secret[i]:
@@ -36,29 +68,29 @@ class Wordle:
                 letter.in_word = True
                 secret[i] = '_'
 
-        # check for yellow letters
+        # check for in_word (yellow) letters using a set to reduce complexity
+        secret_set = set(secret)
         for i in range(self.word_length):
             letter = result[i]
             if letter.in_position:
                 continue
-            for j in range(self.word_length):
-                if secret[j] == letter.character:
-                    secret[j] = '_'
-                    letter.in_word = True
-                    break
+            if letter.character in secret_set:
+                secret_set.remove(letter.character)
+                letter.in_word = True
+
         return result
 
-    # is it solved?
     @property
     def is_solved(self) -> bool:
+        """Check if the word has been solved."""
         return len(self.attempts) > 0 and self.attempts[-1] == self.secret_word
 
-    # remaining attempts
     @property
     def remaining_attempts(self) -> int:
+        """Calculate the remaining attempts."""
         return self.max_attempts - len(self.attempts)
 
-    # can attempt be made?
     @property
     def can_attempt(self) -> bool:
+        """Check if another attempt can be made."""
         return self.remaining_attempts > 0 and not self.is_solved
